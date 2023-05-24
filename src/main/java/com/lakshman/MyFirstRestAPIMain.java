d2fb0e4ab776e613b47924ba67d4427722835d76
KANAKALA Lakshmana <kanakala.rao@soprasteria.com>
2023-05-24 19:24:25 +0530

    uploaded

---
+++ src/main/java/com/lakshman/MyFirstRestAPIMain.java
@@ -0,0 +1,13 @@
+package com.lakshman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstRestAPIMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFirstRestAPIMain.class, args);
+    }
+
+}
---
+++ src/test/java/com/lakshman/restapifirst/RestapiFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakshman.restapifirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
